402fadd9871def9a06a87fb0027c924a0e6c316c
Kim Sandberg <s@ndberg.dk>
2018-10-28 20:39:11 +0100

    Stadig crash... ingen error log

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/MainActivity.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/MainActivity.java
@@ -3,6 +3,9 @@ package erickkim.dtu.dk.affaldsprojekt;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +48,12 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        if(savedInstanceState==null){
-            screen1main screen1 = new screen1main();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragmentContent, screen1)
+       // if(savedInstanceState==null){
+        Fragment startscreen = new screen1main();
+        fragmentTransaction
+                    .add(R.id.fragmentContent, startscreen)
                     .commit();
-        }
+        //}
 
 
     }
