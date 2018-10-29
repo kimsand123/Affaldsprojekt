c8941c6e8f6e2bcfc86c7a1e9b875ff7a8f6587a
Erick <User@10.16.99.128>
2018-10-29 11:57:45 +0100

    Creating DeliveryCode DTO's and DAO's

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/Data_DAO_deliveryCode.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/Data_DAO_deliveryCode.java
@@ -1,4 +1,11 @@
 package erickkim.dtu.dk.affaldsprojekt;
 
+import erickkim.dtu.dk.affaldsprojekt.TEST_Data_Backend.TEST_Database;
+
 public class Data_DAO_deliveryCode {
+
+    public deliveryCode getAvailableDeliveryCode () {
+        Data_DTO_deliveryCode tempCode = new Data_DTO_deliveryCode();
+        TEST_Database.getDeliveryCode;
+    }
 }
--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/TEST_Data_Backend/TEST_Database.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/TEST_Data_Backend/TEST_Database.java
@@ -1,5 +1,7 @@
 package erickkim.dtu.dk.affaldsprojekt.TEST_Data_Backend;
 
+import erickkim.dtu.dk.affaldsprojekt.Data_DTO_deliveryCode;
+
 public class TEST_Database {
 
     static int personCoins[][] = { {657, 323, 252, 623, 696, 111, 112, 113, 999, 986},
@@ -13,6 +15,21 @@ public class TEST_Database {
             }
         }
         return -1;
+    }
+
+    static Data_DTO_deliveryCode[] usedCodes = new Data_DTO_deliveryCode[1000];
+
+    public static int getDeliveryCode() {
+        int min = 1;
+        int max = 9999;
+        int range = (max - min) + 1;
+        double newCode = (int) (Math.random() * range) + min;
+
+        for (int i = 0; i < usedCodes.length; i++) {
+
+        }
+
+
     }
 
 }
