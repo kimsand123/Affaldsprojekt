023312073a3b0dbd99e5c3fb2195eb54d19df523
Kim Sandberg <s@ndberg.dk>
2018-10-29 12:58:01 +0100

    screen3 afterdelivery designet og implementeret ikke testet

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen3afterdelivery.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen3afterdelivery.java
@@ -1,7 +1,5 @@
 package erickkim.dtu.dk.affaldsprojekt;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -11,24 +9,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-public class screen2delivery extends Fragment implements View.OnClickListener {
+public class screen3afterdelivery extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private View root;
+    private Button statisticButton;
+    private TextView txtCoinBox3 ;
+    private TextView txtInfoBox3;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private View root;
-    private Button doneButton;
-    private Button newIdButton;
-    private TextView txtIdBox;
-    private TextView txtCoinBox2;
 
-
-    public screen2delivery() {
+    public screen3afterdelivery() {
         // Required empty public constructor
     }
 
@@ -38,11 +33,11 @@ public class screen2delivery extends Fragment implements View.OnClickListener {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment screen2delivery.
+     * @return A new instance of fragment screen3afterdelivery.
      */
     // TODO: Rename and change types and number of parameters
-    public static screen2delivery newInstance(String param1, String param2) {
-        screen2delivery fragment = new screen2delivery();
+    public static screen3afterdelivery newInstance(String param1, String param2) {
+        screen3afterdelivery fragment = new screen3afterdelivery();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,46 +55,40 @@ public class screen2delivery extends Fragment implements View.OnClickListener {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_screen3afterdelivery, container, false);
+
+        statisticButton = root.findViewById(R.id.statisticButton);
+
+        txtCoinBox3 = root.findViewById(R.id.txtCoinBox3);
+        txtInfoBox3 = root.findViewById(R.id.txtInfoBox3);
+
+        //TODO: Hent data til coin og infobox. ligenu er det bare tekst.
+        txtInfoBox3.setText("blabdalvsd as dlkjf sdalkhjkl laks kljfdk jlsdfa hjkldfjk hlsf hjklffhjk dfhjk fhjk als kjdfhjk fhjk ");
+        txtCoinBox3.setText("GarbageCoins: 3423424");
+
+        //TODO: Hent data til piechart.
+
+        statisticButton.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        root = inflater.inflate(R.layout.fragment_screen2delivery, container, false);
-
-        doneButton = root.findViewById(R.id.doneButton);
-        newIdButton = root.findViewById(R.id.newIdButton);
-        txtIdBox = root.findViewById(R.id.txtIdBox);
-        txtCoinBox2 = root.findViewById(R.id.txtCoinBox2);
-
-        //TODO: Hent data til textfelter. Lige nu sætter jeg dem bare til noget fast.
-        txtCoinBox2.setText("GarbageCoins: 2873291");
-        txtIdBox.setText("ID: 5 2 4 5");
-
-        doneButton.setOnClickListener(this);
-        newIdButton.setOnClickListener(this);
         return root;
-
     }
 
     @Override
     public void onClick(View v) {
 
         switch(v.getId()){
-            case R.id.doneButton:
+            case R.id.statisticButton:
                 //TODO: sæt hent dataflag i preferences, og skift til screen1main fragment
                 getFragmentManager().beginTransaction()
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .replace(R.id.fragmentContent, new screen3afterdelivery())
+                        //.replace(R.id.fragmentContent, new screen4statistik())
                         .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
                         .addToBackStack(null)
                         .commit();
                 break;
-            case R.id.newIdButton:
-                //TODO: Hent nyt id fra backend.
-                //huske det nok skal foregår i en anden tråd
-                break;
-
         }
-
     }
 }
 
