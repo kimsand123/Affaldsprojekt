9ec4ac8705a9d8dfe5b1063d167affa457b2a9ce
Kim Sandberg <s@ndberg.dk>
2018-10-30 10:30:07 +0100

    Burgermenu changed to colors and proper elements
    No functionality for elements.

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/MainActivity.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/MainActivity.java
@@ -91,19 +91,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (id){
+            case R.id.my_profile:
+                break;
+            case R.id.competitions:
+                break;
+            case R.id.feedback:
+                break;
+            case R.id.about:
+                break;
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
