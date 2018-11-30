4566412401500f5b23a466a27ad8567e76b50abb
Kim Sandberg <s@ndberg.dk>
2018-11-30 08:40:10 +0100

    Code oprydning.

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen4statistic.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen4statistic.java
@@ -12,14 +12,7 @@ import com.jjoe64.graphview.GraphView;
 
 
 public class screen4statistic extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private View root;
     private Button becomeBetterButton;
     private TextView txtCoinBox4;
@@ -30,20 +23,9 @@ public class screen4statistic extends Fragment implements View.OnClickListener {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment screen4statistic.
-     */
-    // TODO: Rename and change types and number of parameters
     public static screen4statistic newInstance(String param1, String param2) {
         screen4statistic fragment = new screen4statistic();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,30 +33,19 @@ public class screen4statistic extends Fragment implements View.OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       root = inflater.inflate(R.layout.fragment_screen4statistic, container, false);
-        // Inflate the layout for this fragment
 
-        txtCoinBox4 = root.findViewById(R.id.txtCoinBox4);
         becomeBetterButton = root.findViewById(R.id.hvordanBliverJegBedreButton);
-        statistic = root.findViewById(R.id.statistic);
-
         becomeBetterButton.setOnClickListener(this);
-
-        //TODO: Hent data til statistik og præsentér dem
-
+        statistic = root.findViewById(R.id.statistic);
+        txtCoinBox4 = root.findViewById(R.id.txtCoinBox4);
         txtCoinBox4.setText("GarbageCoins: " + Data_Controller.getInstance().getTrashCoins());
-
         return root;
     }
-
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen7sortingguide.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/screen7sortingguide.java
@@ -9,14 +9,7 @@ import android.webkit.WebView;
 import android.widget.TextView;
 
 public class screen7sortingguide extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private TextView txtCoinBox5;
     private View root;
     private WebView web;
@@ -25,20 +18,9 @@ public class screen7sortingguide extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment screen7sortingguide.
-     */
-    // TODO: Rename and change types and number of parameters
     public static screen7sortingguide newInstance(String param1, String param2) {
         screen7sortingguide fragment = new screen7sortingguide();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -46,10 +28,6 @@ public class screen7sortingguide extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -59,7 +37,6 @@ public class screen7sortingguide extends Fragment {
         web = root.findViewById(R.id.sortguide_Webview);
         txtCoinBox5.setText("GarbageCoins: " + Data_Controller.getInstance().getTrashCoins());
         web.loadUrl("http://www.naestved-affald.dk/min-ordning/dagrenovation/madaffald/");
-        // Inflate the layout for this fragment
         return root;
     }
 
