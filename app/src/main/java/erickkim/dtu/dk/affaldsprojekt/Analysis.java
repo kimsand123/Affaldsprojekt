7dc97225889780d16b90c54e737ea704afcc08a4
Kim Sandberg <s@ndberg.dk>
2019-01-10 20:57:45 +0100

    Text and calculations beginning to take form.

--- app/src/main/java/erickkim/dtu/dk/affaldsprojekt/Analysis.java
+++ app/src/main/java/erickkim/dtu/dk/affaldsprojekt/Analysis.java
@@ -33,23 +33,53 @@ public class Analysis implements I_Analysis {
     private String getFractionComparison (String fraction, int fractionAmountInGrams){
     // tal taget fra
     // https://affald.dk/da/7-10/affald-og-miljo/artikler/255-fremstilling-af-elektronik-7-10.html
-
-
         String text=null;
-
         int number = getRandom();
+        double resultat;
+        String textEnding="";
+        String textStart="";
+
         switch (fraction){
 
             case "Metal":
                 switch(number){
                     case 1:
                         //Mobiltelefon 25% metal
+                        double telefon = 138*.25;
+                        textEnding = "";
+                        textStart = "Du har";
+
+                        if (!(text.equals(""))){
+                            textStart = " Derudover har du";
+                        }
+                        resultat = fractionAmountInGrams/telefon;
+                        if (resultat >= 2.0){
+                            textEnding = "er";
+                        }
+                        text = text + textStart + " afleveret en mængde metal der svarer til hvad man skal bruge for at lave " + resultat + " mobiltelefon" + textEnding;
                         break;
                     case 2:
                         //Cykel 200 dåser jern eller aluminium
+                        int dåse = 16;
+                        textEnding = "el";
+                        textStart = "Du har";
+
+                        double antalDaaser = fractionAmountInGrams/dåse;
+                        resultat = 200/antalDaaser;
+                        if (resultat >= 2.0){
+                            textEnding = "ler";
+                        }
+                        if (!(text.equals(""))) {
+                            textStart = " Du har også";
+                        }
+                        text = text + textStart + " afleveret jern nok til at lave " + resultat + " cyk" + textEnding;
                         break;
                     case 3:
                         //Der spares 2 ton CO2, når 1 ton jern genanvendes
+                        int co2 = 1000000;
+                        resultat = fractionAmountInGrams/co2;
+                        textStart = "Med " + fractionAmountInGrams + "gr, har du";
+                        text = text + textStart + " sparet" + resultat + "t CO2. Det virker som lidt, men hvis alle gør det bliver det til rigtigt meget.";
                         break;
                 }
                 break;
@@ -58,15 +88,33 @@ public class Analysis implements I_Analysis {
                 switch(number){
                     case 1:
                         // Der spares en CO2 emission på 37 kg CO2 pr ton bioAffald
+                        int co2prgram = 37000/1000000;
+                        resultat = fractionAmountInGrams * co2prgram;
+                        textEnding = "";
+                        textStart = "Du har ";
+                        if (!(text.equals(""))) {
+                            textStart = " Du har også";
+                        }
+                        text = text + textStart + " sparet naturen for " + resultat + "t CO2 med dit Bioaffald.";
                         break;
                     case 2:
                         //Et ton bioaffald bliver til 84 normalkubikmeter ren metan.
                         //En gasbus kan køre 44 km på denne mængde gas.
+                        int metanm3prgram = 84/1000000;
+                        resultat = fractionAmountInGrams * metanm3prgram;
+
+                        textStart = "Vidste du at";
+                        if (!(text.equals(""))) {
+                            textStart = " Vidste du også at";
+                        }
+                        text = text + textStart + " en gasbus kan køre  " + 44/84*resultat + "km på den mængde " + resultat + "m3 rene metan du har genereret med dit Bioaffald.";
+
                         break;
                     case 3:
                         //Hvis gassen bruges til at producere elektricitet af, kan der produceres ca. 230 kWh energi pr ton bioaffald
                         //(830 MJ/ton). Det svarer til, at man kan lade en mobiltelefon op i 19 år, eller spille Play Station 4 i 1 år og 4
                         //måneder.
+
                         break;
                 }
                 break;
